a80332773020dbc72f16c6904223930d4738bf08
Shaili083 <shailisrivastava35@gmail.com>
2018-09-05 00:07:34 -0700

    functionality included

--- js/index.js
+++ js/index.js
@@ -1,74 +1,66 @@
 function toggledetail(){
-          var para=document.getElementById('hiddenpara');
-          var ml = document.getElementById('more-less');
-          var dot = document.getElementById('dots-one');
+  
+  var para=document.getElementById('extend');
+if(moreid.innerHTML=="More")
+  {
+  para.classList.toggle("ptag");
+  moreid.innerHTML="back";
 
-          if ( dot.style.display == "inline" ) 
-          {
-            dot.style.display = "none";
-            ml.innerHTML = "Back";
-            para.style.display = "block";
-          }
-          else
-          {
-            dot.style.display = "inline";
-            ml.innerHTML = "More";
-            para.style.display = "none"; 
-          }
-        }
+  }
+  else{
+    moreid.innerHTML="More";
+  }
+
+  
+}
+          
+        
 
 
         function toggledetailtwo(){
-          var paratwo=document.getElementById('hiddenpara-two');
-          var mltwo = document.getElementById('more-less-two');
-          var dottwo = document.getElementById('dots-two');
+          var paratwo=document.getElementById('extendtwo');
+          if(moreidtwo.innerHTML=="More")
+          {
+           paratwo.classList.toggle("ptag");
+           moreidtwo.innerHTML="back";
+         }
+         else
+         {
+         moreidtwo.innerHTML="More";
+         }
 
-          if ( dottwo.style.display == "inline" ) 
-          {
-            dottwo.style.display = "none";
-            mltwo.innerHTML = "Back";
-            paratwo.style.display = "block";
-          }
-          else
-          {
-            dottwo.style.display = "inline";
-            mltwo.innerHTML = "More";
-            paratwo.style.display = "none";
-          }
             }
 
 
 
-          function toggledetailthree(){
-          var parathree=document.getElementById('hiddenpara-three');
-          var mlthree = document.getElementById('more-less-three');
-          var dotthree = document.getElementById('dots-three');
-
-          if ( dotthree.style.display == "inline" ) 
-          {
-            dotthree.style.display = "none";
-            mlthree.innerHTML = "Back";
-            parathree.style.display = "block";
-          }
-          else
-          {
-            dotthree.style.display = "inline";
-            mlthree.innerHTML = "More";
-            parathree.style.display = "none";
-          }
+          function toggledetailthree()
+          { var parathree=document.getElementById('extendthree');
+            if(moreidthree.innerHTML=="More"){
+         
+          parathree.classList.toggle("ptag");
+          moreidthree.innerHTML="back";
+        }
+        else{
+           moreidthree.innerHTML="More";
+         }
+          
 }
 
 
         function showpop(){
-          var pop= document.getElementById('relative-popup');
-          pop.style.display="block";
-        }
-        function hidepop()
-        {
-          var pop=document.getElementById('relative-popup');
-          pop.style.display="none";
-        }
 
+          var pop= document.getElementById('relative-popup');
+          console.log(pop.style.display)
+          if(pop.style.display=="block")
+          {
+            pop.style.display="none";
+          }
+          else{
+            pop.style.display="block";
+          
+        }        
+        // alert("working")
+}
         function currentsearch(){
           var searchbar = document.getElementById('searchfunc');
           searchbar.style.width="300px";
@@ -107,11 +99,21 @@ for (var i= 0; i < btns.length; i++) {
      if(current.length > 0)
      {
     current[0].className = current[0].className.replace(" active", "");}
-    this.className += "active";
+    this.className += " active";
   });
 }
 
 
 
 
-          
+          function callyes() {
+    document.getElementById("Y").checked= true;
+    document.getElementById("N").checked=false;
+     
+}
+function callNo() {
+    document.getElementById("N").checked=true;
+    document.getElementById("Y").checked=false;
+
+}
+
